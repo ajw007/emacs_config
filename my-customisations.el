39ad99a524dce8682ec2794243b015e06a37f81f
Matt Burrows <matt@Matts-MacBook-Nano.local>
2009-07-14 18:42:11 +0100

    Added org mode agenda settings

--- my-customisations.el
+++ my-customisations.el
@@ -202,11 +202,13 @@
       (erc :server "irc.freenode.net" :port 6667 :nick "dic3m4n"))))
 
 ;; Org mode setup
-
 (require 'org-install)
 (add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
 (setq org-log-done t)
-(setq org-agenda-files (list "~/org/todo.org"))
+(setq org-agenda-files (list "~/Dropbox/todo.org"))
+(setq org-todo-keywords '("TODO" "STARTED" "WAITING" "DONE"))
+(setq org-agenda-include-diary t)                            
+(setq org-agenda-include-all-todo t)          
 
 ;; SLIME mode
 (add-to-list 'load-path "~/src/slime/")
