ecd5f7967c808e63c8310c9ebba38c0539e871e5
Matt Burrows <mburrows@mblin.bats.com>
2010-05-12 10:59:33 +0100

    Added highlight-word, extend-selection and bookmarks
    Also added boost test case snippets for C++ mode

--- my-customisations.el
+++ my-customisations.el
@@ -213,6 +213,62 @@
 (setq cua-enable-cua-keys nil)
 (cua-mode t)
 
+;; Setup bookmarks
+(setq 
+  bookmark-default-file "~/elisp/bookmarks" 
+  bookmark-save-flag 1)                     
+
+;; Highlighting to see all occurrences of a word in the buffer
+(setq highlighted-word "")
+(make-variable-buffer-local 'highlighted-word)
+
+(defun highlight-word (word)
+ (interactive (list (thing-at-point 'symbol)))
+ (unhighlight-regexp (regexp-quote highlighted-word))
+ (if (not (equal highlighted-word word))
+     (progn
+       (highlight-regexp (regexp-quote word))
+       (setq highlighted-word word))
+   (setq highlighted-word "")))
+
+(global-set-key (kbd "C-x C-h") 'highlight-word)
+
+;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
+(defun semnav-up (arg)
+  (interactive "p")
+  (when (nth 3 (syntax-ppss))
+    (if (> arg 0)
+        (progn
+          (skip-syntax-forward "^\"")
+          (goto-char (1+ (point)))
+          (decf arg))
+      (skip-syntax-backward "^\"")
+      (goto-char (1- (point)))
+      (incf arg)))
+  (up-list arg))
+
+;; by Nikolaj Schumacher, 2008-10-20. Released under GPL.
+(defun extend-selection (arg &optional incremental)
+  "Select the current word.
+Subsequent calls expands the selection to larger semantic unit."
+  (interactive (list (prefix-numeric-value current-prefix-arg)
+                     (or (and transient-mark-mode mark-active)
+                         (eq last-command this-command))))
+  (if incremental
+      (progn
+        (semnav-up (- arg))
+        (forward-sexp)
+        (mark-sexp -1))
+    (if (> arg 1)
+        (extend-selection (1- arg) t)
+      (if (looking-at "\\=\\(\\s_\\|\\sw\\)*\\_>")
+          (goto-char (match-end 0))
+        (unless (memq (char-before) '(?\) ?\"))
+          (forward-sexp)))
+      (mark-sexp -1))))ex
+
+(global-set-key "\M-8" 'extend-selection)
+
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Language modes
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
